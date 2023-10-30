fa77f26d937a3bad30f455ddd648543b421ea48c
Gilbert <82952946+So-Its-GiL@users.noreply.github.com>
2023-10-30 15:41:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-// Your JSON object
-const json =     {
+// Updated JSON data
+const newData = [
+    {
+        "Id": "12246013",
+        "PartNumber": "0464234009",
+        "ManufacturerName": "TE CONNECTIVITY / RAYCHEM",
+        "LeadTime": "7",
+        "InventoryList": [],
+        "MinOrderQuantity": "1",
+        "StandardPackQuantity": "1",
+        "StockQuantity": 0,
+        "HTS": "3926909090",
+        "UOM": "FT",
+        "Rohs": "Y",
+        "ECCN": null,
+        "DataSheetLink": "null",
+        "LargeImage": "null",
+        "Description": "MIL-LT-3/32-0-SP-CS7227",
+        "PriceList": []
+    },
+    {
         "Id": "12246053",
         "PartNumber": "050671-000",
         "ManufacturerName": "TE CONNECTIVITY / RAYCHEM",
@@ -16,25 +35,34 @@ const json =     {
         "LargeImage": "https://www.te.com/catalog/common/images/PartImages/prso1solderslve.jpg",
         "Description": "null",
         "PriceList": []
-    },
+    }
+];
 
 // Get the output element from the HTML document
 const outputElement = document.getElementById("output");
 
-// Create HTML content using the values from the JSON object
-const htmlContent = `
-  <img src="${LargeImage}" alt="Product Image" />
-    <p>ID: ${json.Id}</p>
-    <p>Part Number: ${json.PartNumber}</p>
-    <p>Manufacturer Name: ${json.ManufacturerName}</p>
-    <p>Lead Time: ${json.LeadTime}</p>
-    <p>Min Order Quantity: ${json.MinOrderQuantity}</p>
-    <p>Standard Pack Quantity: ${json.StandardPackQuantity}</p>
-    <p>Stock Quantity: ${json.StockQuantity}</p>
-    <p>UOM: ${json.UOM}</p>
-    <p>Data Sheet Link: ${json.DataSheetLink}</p>
-    <p>Description: ${json.Description}</p>
-`;
+// Create HTML content using the values from the updated JSON data
+let htmlContent = '';
+newData.forEach(item => {
+    htmlContent += `
+        <div>
+            <p>ID: ${item.Id}</p>
+            <p>Part Number: ${item.PartNumber}</p>
+            <p>Manufacturer Name: ${item.ManufacturerName}</p>
+            <p>Lead Time: ${item.LeadTime}</p>
+            <p>Min Order Quantity: ${item.MinOrderQuantity}</p>
+            <p>Standard Pack Quantity: ${item.StandardPackQuantity}</p>
+            <p>Stock Quantity: ${item.StockQuantity}</p>
+            <p>HTS: ${item.HTS}</p>
+            <p>UOM: ${item.UOM}</p>
+            <p>Rohs: ${item.Rohs}</p>
+            <p>Data Sheet Link: <a href="${item.DataSheetLink}" target="_blank">${item.DataSheetLink}</a></p>
+            <p>Large Image: <img src="${item.LargeImage}" alt="Large Image"></p>
+            <p>Description: ${item.Description}</p>
+        </div>
+        <hr>
+    `;
+});
 
 // Set the innerHTML of the output element with the created HTML content
 outputElement.innerHTML = htmlContent;
